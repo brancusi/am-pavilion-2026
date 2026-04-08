5430a8f5c2767564e68e0557fdb0ac558b3df7df
Aram Zadikian <brancusi@gmail.com>
2026-04-08 15:25:00 -0700

    wip

--- dev/mockups/blue_yellow_blue_yellow.cljs
+++ dev/mockups/blue_yellow_blue_yellow.cljs
@@ -7,34 +7,64 @@
   ;; Original data pulled from Firebase "mockup-002"
   ;; Modify and evaluate the set-edn call below to push changes.
 
-  (set-edn "blue-yellow-blue-yellow"
+  (set-edn "blue-yellow-blue-yellow-002"
            {:name "Blue Yellow Blue Yellow"
             :camera {:position [210 210 210]
                      :target [0 20 0]}
             :data [{:bounds [36 36 48]
-                    :layers [[{:color "yellow"
+                    :layers [[{:color "#FFE15D"
                                :align :tl
                                :dims [4 4 6]}
-                              {:color "yellow"
+                              {:color "#faafe9"
                                :align :tr
                                :dims [4 4 6]}
-                              {:color "yellow"
+                              {:color "#F16095"
                                :align :bl
                                :dims [4 4 6]}
-                              {:color "yellow"
+                              {:color "white"
                                :align :br
                                :dims [4 4 6]}]]}
-                   {:layers [[{:color "turquoise"
+                   {:layers [[{:color "#00F6DE"
                                :align :tl
                                :dims [36 36 48]}]]}
-                   {:layers [[{:color "yellow"
+                   {:layers [[{:color "#faafe9"
                                :align :tl
                                :dims [36 6 12]}]]}
-                   {:layers [[{:color "turquoise"
-                               :align :tl
+                   {:layers [[{:color "#F16095"
+                               :align :tr
                                :dims [12 6 12]}]]}
-                   {:layers [[{:color "yellow"
+                   {:layers [[{:color "#FFE15D"
+                               :align :tr
+                               :dims [4 4 4]}]]}]})
+
+  (set-edn "blue-yellow-blue-yellow-003"
+           {:name "Blue Pink Red Yellow - Confirmed«"
+            :camera {:position [210 210 210]
+                     :target [0 20 0]}
+            :data [{:bounds [36 36 48]
+                    :layers [[{:color "#FFE15D"
                                :align :tl
+                               :dims [4 4 6]}
+                              {:color "#faafe9"
+                               :align :tr
+                               :dims [4 4 6]}
+                              {:color "#F16095"
+                               :align :bl
+                               :dims [4 4 6]}
+                              {:color "white"
+                               :align :br
+                               :dims [4 4 6]}]]}
+                   {:layers [[{:color "#00F6DE"
+                               :align :tl
+                               :dims [36 36 48]}]]}
+                   {:layers [[{:color "#faafe9"
+                               :align :tl
+                               :dims [36 6 12]}]]}
+                   {:layers [[{:color "#F16095"
+                               :align :tr
+                               :dims [12 6 12]}]]}
+                   {:layers [[{:color "#FFE15D"
+                               :align :tr
                                :dims [4 4 4]}]]}]})
 
   ;; end comment
